913c055372136262fec8445993e395644a6d7f0b
tnazarro <tnazarro@dropletmeasurement.com>
2024-10-30 15:55:28 -0600

    Fixed bug in main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 with open("/home/tnazarro/workProjects/github.com/tnazarro/bookbot/books/frankenstein.txt") as f:
     file_contents= f.read()
 
-def main()
+def main():
     print(file_contents)
 
 main()
